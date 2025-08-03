4e99113d6538080a0a15eec9ed66dc460ee1db87
Dickson-025 <dicksonmendis2501@gmail.com>
2025-08-03 15:38:49 +0530

    change db to mongo atlass

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const multer = require('multer');
 const fs = require('fs');
 const pdfparse = require('pdf-parse');
 const axios = require('axios');
+const mongoose = require('mongoose');
+const { type } = require('os');
+const { timeStamp } = require('console');
 
 dotenv.config()
 
@@ -20,21 +23,36 @@ app.use(express.json());
 // const openai = new OpenAI({apiKey: process.env.OPEN_AI_KEY})
 // console.log('here openai key', openai);
 
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connection Successfull'))
+.catch((error) => console.log('Connection Failed', error))
 
-let db;
-;(async ()=>{
-    try {
-        db = await mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: 'root',
-            database: "chat_resume"
-        })
-        console.log('Database Connected Successfully');
-    } catch (error) {
-        console.log('Database Failed to Connect', error);
-    }
-})()
+const { Schema, model } = mongoose;
+const mongoSchema = new Schema({
+    filename: {type: String, required: true},
+    content: {type: String, required: true},
+    contenttext: {type: String, required: true},
+}, {timeStamp: true});
+
+const Resume = model('resume', mongoSchema)
+
+// let db;
+// ;(async ()=>{
+//     try {
+//         db = await mysql.createConnection({
+//             host: "localhost",
+//             user: "root",
+//             password: 'root',
+//             database: "chat_resume"
+//         })
+//         console.log('Database Connected Successfully');
+//     } catch (error) {
+//         console.log('Database Failed to Connect', error);
+//     }
+// })()
 
 const upload = multer({'dest': "/tmp/upload"});
 
@@ -52,18 +70,26 @@ app.post("/api/upload", upload.single("file"), async (req, res) => {
         // const parsedpdf = await pdfparse(await fs.promises.readFile(req.file.path));
         const fileBuffer = await fs.promises.readFile(req.file.path);
         const parsedpdf = await pdfparse(fileBuffer);
-        const [result] = await db.execute("INSERT INTO resume (filename, content, contenttext) VALUES (?, ?, ?)", [
-            req.file.originalname,
-            fileBuffer.toString('base64'),
-            parsedpdf.text
-        ])
+        const result = new Resume({
+            filename: req.file.originalname,
+            content: fileBuffer.toString('base64'),
+            contenttext: parsedpdf.text
+        })
+
+        const saved = await result.save();
+
+        // const [result] = await db.execute("INSERT INTO resume (filename, content, contenttext) VALUES (?, ?, ?)", [
+        //     req.file.originalname,
+        //     fileBuffer.toString('base64'),
+        //     parsedpdf.text
+        // ])
         // console.log(result.insertId);
         
         res.status(200).json({
             status: "Success",
             message: 'File Uploaded Successfully',
             data: {
-                id: result.insertId
+                id: saved._id
             }
         })
     } catch (error) {
@@ -80,8 +106,9 @@ app.post('/api/ask/:id', async(req, res) => {
     try {
         const id = req.params.id;
         const question = req.body.chatinput || '';
-        const [result] = await db.execute("SELECT contenttext FROM resume where id = ?", [id]);
-        const pdftext = result[0]?.contenttext || '';
+        // const [result] = await db.execute("SELECT contenttext FROM resume where id = ?", [id]);
+        const resume = await Resume.findById(id);
+        const pdftext = resume?.contenttext || '';
         const response = await axios.post('https://api.groq.com/openai/v1/chat/completions',{
             model: "meta-llama/llama-4-scout-17b-16e-instruct",
             messages: [
@@ -137,19 +164,22 @@ app.post('/api/ask/:id', async(req, res) => {
 app.get("/api/files/:id", async (req, res) => {
     try {
         const id = req.params.id;
-        const [result] = await db.execute("SELECT * FROM resume WHERE id = ?", [id]);
-        if(result.length == 0) return res.status(404).json({ message: "File not found."})
+        const resume = await Resume.findById(id);
+        if(!resume) return res.status(404).json({ message: "File not found."})
+
+        // const [result] = await db.execute("SELECT * FROM resume WHERE id = ?", [id]);
+        // if(result.length == 0) return res.status(404).json({ message: "File not found."})
         // console.log("Get Result", result);
         res.setHeader('Cache-Control', 'no-store');
-        res.setHeader("Content-Disposition", `attachment; filename="${result[0].filename}"`);
+        res.setHeader("Content-Disposition", `attachment; filename="${resume.filename}"`);
         res.setHeader("Content-Type", "application/pdf");
-        res.setHeader("X-Filename", result[0].filename);
+        res.setHeader("X-Filename", resume.filename);
         res.setHeader("Access-Control-Expose-Headers", "X-Filename");
         let fileContent;
-        if (typeof result[0].content === "string") {
-            fileContent = Buffer.from(result[0].content, "base64");
+        if (typeof resume.content === "string") {
+            fileContent = Buffer.from(resume.content, "base64");
         } else {
-            fileContent = result[0].content;
+            fileContent = resume.content;
         }
         res.status(200).send(fileContent);
         // res.status(200).json({
